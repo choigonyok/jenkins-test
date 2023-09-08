26c7597a8fdb3c40bb700a6a439e8c952aed3e66
최윤석 <choigonyok@MACBOOK.local>
2023-09-08 21:46:40 +0900

    TEST

--- sum/sum.go
+++ sum/sum.go
@@ -15,7 +15,44 @@ func Sub(a, b int) int {
 	return c
 }
 
-func AA(a, b int) int {
-	c := a * b
-	return c
-}
+// go plugin 설치
+// Tool에서 golang 버전 적용
+//sudo snap install openjdk
+
+// pipeline {
+//     agent any
+//     tools {
+//         go 'golang'
+//     }
+
+//     stages {
+//         stage('Checkout') {
+//             steps {
+//                 git branch: 'main',
+//                     credentialsId: '4467cd03-f3e0-4d7f-b6b4-4ecfe57568fa',
+//                     url: 'https://github.com/choigonyok/jenkins-test.git'
+//             }
+//         }
+//         stage('Test') {
+//             steps {
+//                     echo 'Running test'
+//                     sh 'cd sum && go test -cover .'
+//             }
+//         }
+//     }
+// }
+
+// sudo apt update
+// sudo apt install openjdk-17-jre
+// java -version
+// openjdk version "17.0.7" 2023-04-18
+// OpenJDK Runtime Environment (build 17.0.7+7-Debian-1deb11u1)
+// OpenJDK 64-Bit Server VM (build 17.0.7+7-Debian-1deb11u1, mixed mode, sharing)
+
+// curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+//   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+// echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+//   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+//   /etc/apt/sources.list.d/jenkins.list > /dev/null
+// sudo apt-get update
+// sudo apt-get install jenkins
